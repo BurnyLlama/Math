92eb6705c213d5d4984866b85328d1c5a52f9f9f
BurnyLlama <37349525+BurnyLlama@users.noreply.github.com>
2019-02-06 20:29:31 +0000

    Added a the possibility to change pi.

--- Math/js.js
+++ Math/js.js
@@ -1,15 +1,21 @@
+//redefine pi
+var pi = 3.14;
+function definePi() {
+  pi = prompt('What do you want your new pi as?')
+};
+
 //spheare
 function spheareArea() {
   var r = prompt('What is the radious? (Exclude units! cm, miles, km, feet, etc.) The answer will be in the same unit, but squared.');
-  var answer = 4 * 3.14 * r * r;
+  var answer = 4 * pi * r * r;
   var ctx = document.getElementById('history').innerHTML;
-  document.getElementById('history').innerHTML = "4 * 3.14 * " + r + "² = " + answer + "<br>" + ctx;
+  document.getElementById('history').innerHTML = "4 * " + pi + " * " + r + "² = " + answer + "<br>" + ctx;
 };
 function spheareVolume() {
   var r = prompt('What is the radious? (Exclude units! cm, miles, km, feet, etc.) The answer will be in the same unit, but cubed.');
-  var answer = 4 * 3.14 * r * r * r / 4;
+  var answer = 4 * pi * r * r * r / 4;
   var ctx = document.getElementById('history').innerHTML;
-  document.getElementById('history').innerHTML = "4 * 3.14 * " + r + "³ / 4 = " + answer + "<br>" + ctx;
+  document.getElementById('history').innerHTML = "4 * " + pi + " * " + r + "³ / 4 = " + answer + "<br>" + ctx;
 };
 
 
@@ -17,14 +23,14 @@ function spheareVolume() {
 function coneArea() {
   var r = prompt('What is the radious? (Exclude units! cm, miles, km, feet, etc.) The answer will be in the same unit, but squared.');
   var s = prompt('What is the length of the side? (Not height!) (Exclude units! cm, miles, km, feet, etc.) The answer will be in the same unit, but squared.');
-  var answer = 3.14 * r * s;
+  var answer = pi * r * s;
   var ctx = document.getElementById('history').innerHTML;
-  document.getElementById('history').innerHTML = "3.14 * " + r + " * " + s + " = " + answer + "<br>" + ctx;
+  document.getElementById('history').innerHTML = pi +" * " + r + " * " + s + " = " + answer + "<br>" + ctx;
 };
 function coneVolume() {
   var r = prompt('What is the radious? (Exclude units! cm, miles, km, feet, etc.) The answer will be in the same unit, but squared.');
   var h = prompt('What is the height? (Not thelength of the side!) (Exclude units! cm, miles, km, feet, etc.) The answer will be in the same unit, but squared.');
-  var answer = 3.14 * r * r * h / 3;
+  var answer = pi * r * r * h / 3;
   var ctx = document.getElementById('history').innerHTML;
-  document.getElementById('history').innerHTML = "3.14  * " + r + "² * " + h + " / 3 = " + answer + "<br>" + ctx;
+  document.getElementById('history').innerHTML = pi + " * " + r + "² * " + h + " / 3 = " + answer + "<br>" + ctx;
 };
